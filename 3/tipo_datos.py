86edea011ac22ceea3b5f97202935302fc83895f
adrianco79 <acorrea@unne.edu.ar>
2024-02-23 12:05:28 -0300

    operadores
    Operadores

--- 3/tipo_datos.py
+++ 3/tipo_datos.py
@@ -1,2 +1,10 @@
-cadena = input("ingrese un valor")
+cadena = (input("ingrese un valor:")) #permite ingresar datos por teclado ingresa como str
+numero=int(input("ingrese un valor:"))  #parseo el tipo de dato
+
+decimal=float(input("ingrese un decimal: "))
 print("El valor ingresaso es:",cadena)
+print(type(cadena))
+
+print(type(numero)) #permite imprimer el tipo de dato
+
+print(type(decimal)) #permite imprimer el tipo de dato
---
+++ 4/operadores.py
@@ -0,0 +1,24 @@
+x=int(input("ingrese un valor:"))
+y=int(input("ingrese otro valor:"))
+print("la suma es",x+y)
+print("la resta es",x-y)
+print("la multiplicacion es",x*y)
+print("la division es",x / y)
+
+print("la division resultado entero es",x //  y)
+
+print("el exponente es",x ** y)
+
+#Operadores comparativos
+
+#Mayor que
+print("x es mayor que y", x>y)
+
+#Menor que
+print("x es menor que y", x<y)
+
+#Igual a
+print("x es igual que y", x==y)
+
+#No es igual a
+print("x es igual que y", x!=y)
